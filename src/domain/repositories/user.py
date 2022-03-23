18a5a062de3b86458d47b96140d25ffc83b35675
Jaeseob Hwang <jetsbee@gmail.com>
2022-03-23 15:43:14 +0900

    Fix

--- src/domain/repositories/user.py
+++ src/domain/repositories/user.py
@@ -4,4 +4,4 @@ from abc import ABC, abstractmethod
 class UserRepository(ABC):
     @abstractmethod
     def save(self):
-        raise NotImplementedError("Method is not implemented in class.")
+        pass
