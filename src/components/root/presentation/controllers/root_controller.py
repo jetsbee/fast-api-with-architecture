4e8a19d7e1ffaf29cf8f9870bc01da3c07826f0e
Jaeseob Hwang <jetsbee@gmail.com>
2022-03-25 15:15:25 +0900

    Async request api in api

--- src/components/root/presentation/controllers/root_controller.py
+++ src/components/root/presentation/controllers/root_controller.py
@@ -1,8 +1,38 @@
+from typing import Union, Dict, Coroutine
+
+import aiohttp
+
 from fastapi import APIRouter
 
 router = APIRouter()
 
 
+async def get_root_through_network() -> Union[Dict, Coroutine, None]:
+    rst = None
+    async with aiohttp.ClientSession() as session:
+        async with session.get("http://localhost:8000/") as resp:
+            try:
+                rst = await resp.json()
+            except Exception as exc:
+                import traceback
+
+                print("#Poor_Logging_For_Only_Sample_Purpose:", exc)
+                traceback.print_exc()
+                print("#End_Of_Poor_Logging")
+
+    return rst
+
+
 @router.get("/")
 async def root():
     return {"message": "Server is running."}
+
+
+@router.get("/foo")
+async def foo():
+    root_data = await get_root_through_network()
+
+    if type(root_data) is dict:
+        root_data["foo_additional"] = "foo"
+
+    return root_data
