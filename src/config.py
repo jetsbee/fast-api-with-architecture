892b4722c6b70883c38d35bfcf7f0d3333243c3c
Jaeseob Hwang <jetsbee@gmail.com>
2022-03-26 01:49:00 +0900

    Develop config system

--- src/config.py
+++ src/config.py
@@ -1,16 +1,50 @@
+from pathlib import Path
 from functools import lru_cache
 
 from pydantic import BaseSettings
 
 
-class Settings(BaseSettings):
-    SECRET_KEY: str = "c175fcc672c7a6eb4fb53dcbd95b4e8d043018d54926e97badcfb4bd2642cbac"
+class GlobalSettings(BaseSettings):
+    ENV_STATE: str = "dev"
+    # JWT
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRES: int = 30 * 60  # minutes
     REFRESH_TOKEN_EXPIRES: int = 15 * 86400  # days
     AUTH_HEADER_TYPE: str = "bearer"
+    # End of JWT
+
+    class Config:
+        env_file = str(Path(__file__).resolve().parent / "envs" / ".env")
+
+
+class RequiredSettings(BaseSettings):
+    # JWT
+    SECRET_KEY: str
+    # End of JWT
+
+
+class DevSettings(GlobalSettings, RequiredSettings):
+    class Config:
+        env_file = str(Path(__file__).resolve().parent / "envs" / "dev.env")
+
+
+class ProdSettings(GlobalSettings, RequiredSettings):
+    class Config:
+        env_file = str(Path(__file__).resolve().parent / "envs" / "prod.env")
+
+
+class FactorySettings:
+    @staticmethod
+    def load():
+        env_state = GlobalSettings().ENV_STATE
+        if env_state == "dev":
+            return DevSettings()
+        elif env_state == "prod":
+            return ProdSettings()
+        else:
+            raise Exception("Check the ENV_STATE.")
 
 
 @lru_cache()
 def get_settings():
-    return Settings()
+    return FactorySettings.load()
