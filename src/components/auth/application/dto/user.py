99791e824c778cdaa5dd5933227d58e51bfbd944
Jaeseob Hwang <jetsbee@gmail.com>
2022-04-07 14:32:59 +0900

    Refactor

--- src/components/auth/application/dto/user.py
+++ src/components/auth/application/dto/user.py
@@ -6,12 +6,12 @@ from ...domain.models.user import UserModel
 class UserBase(BaseModel):
     username: str
 
+    def to_model(self) -> UserModel:
+        return UserModel(**self.dict())
+
 
 class UserIn(UserBase):
     password: str
-
-    def to_model(self) -> UserModel:
-        return UserModel(**self.dict())
 
 
 class UserOut(UserBase):
@@ -20,6 +20,3 @@ class UserOut(UserBase):
     @classmethod
     def from_model_type(cls, user: UserModel) -> BaseModel:
         return cls(**user.dict())
-
-    def to_model(self) -> UserModel:
-        return UserModel(**self.dict())
