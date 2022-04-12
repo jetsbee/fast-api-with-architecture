c84abd664e59b55ed0b9c1846e7995e911f05292
Jaeseob Hwang <jetsbee@gmail.com>
2022-04-12 11:54:20 +0900

    Add logging decorator as mock

--- src/error/handlers.py
+++ src/error/handlers.py
@@ -6,18 +6,22 @@ from fastapi.exception_handlers import (
 from fastapi.responses import JSONResponse
 
 from .exceptions import StarletteHTTPException, RequestValidationError, APIException
+from ..logging import add_exc_logger
 
 
+@add_exc_logger
 async def custom_http_exception_handler(request: Request, exc: StarletteHTTPException):
     # print(f"OMG! An HTTP error!: {repr(exc)}")
     return await http_exception_handler(request, exc)
 
 
+@add_exc_logger
 async def validation_exception_handler(request: Request, exc: RequestValidationError):
     # print(f"OMG! The client sent invalid data!: {exc}")
     return await request_validation_exception_handler(request, exc)
 
 
+@add_exc_logger
 async def api_exception_exception_handler(request: Request, exc: APIException):
     # print("#PoorSimpleLoggerForOnlySamplePurpose:", exc)
     return JSONResponse(
---
+++ src/logging.py
@@ -0,0 +1,31 @@
+import logging
+from functools import wraps
+from typing import Callable, Awaitable
+
+from fastapi import Request
+from fastapi.logger import logger
+from starlette.responses import Response
+
+"""
+# Logging issue under uvicorn
+
+logger.debug(), logger.info() is not working, even if do logger.setLevel()
+
+- Ref 1. https://github.com/tiangolo/fastapi/issues/2019#issuecomment-687845486
+- Ref 2. https://github.com/encode/uvicorn/issues/945#issuecomment-819692145
+"""
+# Todo: specify level with config or env
+logging.basicConfig(level=logging.INFO)  # Set all of loggers level
+
+ErrorHanderCallable = Callable[[Request, Exception], Awaitable[Response]]
+
+
+def add_exc_logger(original_function: ErrorHanderCallable) -> ErrorHanderCallable:
+    @wraps(original_function)
+    async def wrapper(request: Request, exc: Exception) -> Awaitable[Response]:
+        logger.info("Hello, info logger!")
+        logger.error("Hello, error logger!")
+        response = await original_function(request, exc)
+        return response
+
+    return wrapper
